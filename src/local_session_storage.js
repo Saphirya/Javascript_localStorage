0dbeb59efaaa1b980cba14cb49b24b8cda892ba8
Florian <floriangeorges1992@gmail.com>
2024-12-23 22:15:08 +0100

    update

--- src/local_session_storage.js
+++ src/local_session_storage.js
@@ -1,16 +1,39 @@
 // localStorage.setItem("theme", "dark");
 // console.log(localStorage.getItem("theme"));
-console.log(localStorage.key(0));
-for (let i = 0; i < localStorage.length; i++) {
-  console.log(localStorage.key(i));
-}
+// console.log(localStorage.key(0));
+// for (let i = 0; i < localStorage.length; i++) {
+//   console.log(localStorage.key(i));
+// }
 
-for (let key in localStorage) {
-  console.log(key, ":", localStorage[key]);
-}
+// for (let key in localStorage) {
+//   console.log(key, ":", localStorage[key]);
+// }
 
 // localStorage.setItem(
 //   "location",
 //   JSON.stringify({ country: "France", city: "Paris" })
 // );
-console.log(localStorage.getItem("location"));
+// console.log(localStorage.getItem("location"));
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const value = localStorage.getItem("value");
+if (value) {
+  input.value = value;
+}
+
+button.addEventListener("click", () => {
+  localStorage.setItem("value", input.value);
+});
+
+input.addEventListener("input", () => {
+  localStorage.setItem("value", input.value);
+});
+
+//intercepter les modifications du localStorage
+window.addEventListener("storage", (e) => {
+  console.log(e.key, e.newValue);
+  if (e.key === "value") {
+    input.value = e.newValue;
+  }
+});
